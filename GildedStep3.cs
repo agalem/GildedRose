b18a98877fd86c754e6aeb6e574ce4298372a890
ronek22 <jronek3010@gmail.com>
2019-01-06 15:36:23 +0100

    Added Step3

---
+++ GildedStep3.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+
+namespace csharpcore
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+		IList<string> ItemsThatDoesntGetOlder;
+		IList<string> ItemsThatIncreaseQuality;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+            ItemsThatIncreaseQuality = new List<string>
+            {
+                "Aged Brie",
+                "Backstage passes to a TAFKAL80ETC concert"
+            };
+            ItemsThatDoesntGetOlder = new List<string>
+            {
+                "Sulfuras, Hand of Ragnaros"
+            };
+        }
+        private Boolean isUnderHighestQualityValue(Item item) {
+            return item.Quality < 50;
+        }
+        private Boolean isAboveLowestQualityValue(Item item) {
+            return item.Quality > 0;
+        }
+        private void IncreaseQualityValue(Item item)
+        {
+            if (isUnderHighestQualityValue(item))
+            {
+                item.Quality += GetBaseQuality(item);
+            }
+        }
+        private void DecreaseQualityValue(Item item)
+        {
+            if (isAboveLowestQualityValue(item))
+            {
+                item.Quality -= GetBaseQuality(item);
+            }
+        }
+
+        private int GetBaseQuality(Item item){
+            return (item.SellIn >= 0) ? 1 : 2;
+        }
+
+        private void NormalUpdate(Item item) 
+        {
+            DecreaseQualityValue(item);
+        }
+
+        private void BrieUpdate(Item item)
+        {
+            IncreaseQualityValue(item);
+        }
+
+        private void SulfurasUpdate(Item item)
+        {
+
+        }
+
+        private void BackstageUpdate(Item item)
+        {
+            if(item.SellIn < 10) 
+            {
+                IncreaseQualityValue(item);
+            }
+
+            if(item.SellIn < 5) 
+            {
+                IncreaseQualityValue(item);
+            }
+
+            if (item.SellIn < 0) 
+            {
+                item.Quality = 0;
+            }
+        }
+
+        public void IncreasingItemsUpdate(Item item) {
+            IncreaseQualityValue(item);
+            if(item.name == "Backstage passes to a TAFKAL80ETC concert"){
+                BackstageUpdate(item);
+            }
+        }
+
+        public void UpdateQuality()
+        {
+            foreach(Item item in Items)
+            {
+
+                if(!ItemsThatDoesntGetOlder.Contains(item.Name)) {
+                    item.SellIn = item.SellIn - 1;
+                }
+
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    SulfurasUpdate(item);
+                } 
+                else if (ItemsThatIncreaseQuality.Contains(item.Name))
+                {
+                    IncreasingItemsUpdate(item);
+                }
+                else
+                {
+                    NormalUpdate(item);
+                }
+            }
+        }
+    }
+}
